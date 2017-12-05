91f0dfcf437a76db75bee70dfb2dfbd2033a252b
Jeff <morbius1st@gmail.com>
2017-12-04 21:30:40 -0800

    update

--- Windows/Ribbon.cs
+++ Windows/Ribbon.cs
@@ -93,33 +93,43 @@ namespace RevitWindows
 					ribbonPanel = app.CreateRibbonPanel(tabName, panelName);
 				}
 
-				//add a split pull down button to the panel
-				if (!AddStackedComboBoxes(ribbonPanel))
+
+				//add a stacked pair of stacked pull down buttons to the panel
+				if (!AddStackedPullDownhButtons(ribbonPanel))
 				{
-					TaskDialog td = new TaskDialog("Revit Windows");
-					td.TitleAutoPrefix = false;
-					td.MainInstruction = "Failed to Add the Stacked ComboBoxes!";
-					td.MainIcon = TaskDialogIcon.TaskDialogIconWarning;
-					td.CommonButtons = TaskDialogCommonButtons.Ok;
-
-					td.Show();
-
-//					// create the split button failed
-//					MessageBox.Show("Failed to Add the Stacked ComboBoxes!", "Information",
-//						MessageBoxButtons.OK, MessageBoxIcon.Exclamation, MessageBoxDefaultButton.Button1);
+					// create the stacked split button failed
+					MessageBox.Show("Failed to Add the Stacked Push Buttons!", "Information",
+						MessageBoxButtons.OK, MessageBoxIcon.Exclamation, MessageBoxDefaultButton.Button1);
 					return Result.Failed;
 				}
 
+				return Result.Succeeded;
 
+
+
+				//				//add a split pull down button to the panel
+				//				if (!AddStackedComboBoxes(ribbonPanel))
+				//				{
+				//					TaskDialog td = new TaskDialog("Revit Windows");
+				//					td.TitleAutoPrefix = false;
+				//					td.MainInstruction = "Failed to Add the Stacked ComboBoxes!";
+				//					td.MainIcon = TaskDialogIcon.TaskDialogIconWarning;
+				//					td.CommonButtons = TaskDialogCommonButtons.Ok;
 				//
-
+				//					td.Show();
+				//
+				//					// create the split button failed
+				//					MessageBox.Show("Failed to Add the Stacked ComboBoxes!", "Information",
+				//						MessageBoxButtons.OK, MessageBoxIcon.Exclamation, MessageBoxDefaultButton.Button1);
+				//					return Result.Failed;
+				//				}
+				//
 				//				// example
 				//				// add a button to the panel
 				//				ribbonPanel.AddItem(
 				//					createButton("ModifyPoints1", "Modify\nPoints", "ModifyPoints",
 				//						"Modify the points of a topography surface", SMALLICON, LARGEICON));
-
-
+				//
 				//				// example 1
 				//				//add a split pull down button to the panel
 				//				if (!AddPullDownButton(ribbonPanel))
@@ -150,7 +160,7 @@ namespace RevitWindows
 				//					return Result.Failed;
 				//				}
 
-				return Result.Succeeded;
+
 
 			}
 			catch (Exception e)
@@ -170,192 +180,248 @@ namespace RevitWindows
 			uiApp = new UIApplication(app);
 
 		}
-
-		// add a pair of combo boxes - first is the function selection and 
-		// the second is basically a check box replacement since revit
-		// does not have a check box and I don't feel like making one at the moment
-		private bool AddStackedComboBoxes(RibbonPanel rp)
+		//
+		//		// add a pair of combo boxes - first is the function selection and 
+		//		// the second is basically a check box replacement since revit
+		//		// does not have a check box and I don't feel like making one at the moment
+		//		private bool AddStackedComboBoxes(RibbonPanel rp)
+		//		{
+		//			try
+		//			{
+		//				ComboBoxData cbxData1 = new ComboBoxData("functions");
+		//				ComboBoxData cbxData2 = new ComboBoxData("autoactivate");
+		//
+		//				IList<RibbonItem> ris = rp.AddStackedItems(cbxData1, cbxData2);
+		//
+		//				ComboBox cbx0 = ris[0] as ComboBox;
+		//				ComboBox cbx1 = ris[1] as ComboBox;
+		//
+		//				cbx0.ItemText = "combobox 0";
+		//				cbx0.ToolTip = "select a function";
+		//				cbx0.LongDescription = "select a window organize function";
+		//
+		//				cbx1.ItemText = "combobox 1";
+		//				cbx1.ToolTip = "toggle auto activate";
+		//				cbx1.LongDescription = " toggle auto activate on or off";
+		//
+		//				CreateFunctionsCbx(ref cbx0);
+		//				CreateAutoActivateCbx(ref cbx1);
+		//
+		//				cbx0.DropDownClosed += Cbx0_DropDownClosed;
+		//				cbx1.DropDownClosed += Cbx1_DropDownClosed;
+		//
+		//			}
+		//			catch
+		//			{
+		//				return false;
+		//			}
+		//			return true;
+		//		}
+		//
+		//		private void Cbx1_DropDownClosed(object sender, 
+		//			Autodesk.Revit.UI.Events.ComboBoxDropDownClosedEventArgs e)
+		//		{
+		//			TaskDialog.Show("Auto Activate", "this is a test " + ((ComboBox) sender).Current.ItemText
+		//				+ " name (" + ((ComboBox) sender).Current.Name + ")");
+		//		}
+		//
+		//		private void Cbx0_DropDownClosed(object sender, 
+		//			Autodesk.Revit.UI.Events.ComboBoxDropDownClosedEventArgs e)
+		//		{
+		//			TaskDialog.Show("Auto Activate", "this is a test " + ((ComboBox) sender).Current.ItemText
+		//				+ " name (" + ((ComboBox) sender).Current.Name + ")");
+		//		}
+		//
+		//
+		//		private void CreateFunctionsCbx(ref ComboBox cbx)
+		//		{
+		//			cbx.AddItem(createCbxMemberData("A", "Proper Cascade", SMALLICON));
+		//			cbx.AddItem(createCbxMemberData("B", "Window Cascade", SMALLICON));
+		//			cbx.AddItem(createCbxMemberData("C", "Active at Right", SMALLICON));
+		//			cbx.AddItem(createCbxMemberData("D", "Active at Top", SMALLICON));
+		//			cbx.AddItem(createCbxMemberData("E", "Active at Left", SMALLICON));
+		//			cbx.AddItem(createCbxMemberData("F", "Active at Bottom", SMALLICON));
+		//		}
+		//
+		//		private void CreateAutoActivateCbx(ref ComboBox cbx)
+		//		{
+		//			cbx.AddItem(createCbxMemberData("cx0", "Auto Activate On", SMALLICON));
+		//			cbx.AddItem(createCbxMemberData("cx1", "Auto Activate Off", SMALLICON));
+		//		}
+		//
+		//		private ComboBoxMemberData createCbxMemberData(string internalName, 
+		//			string visibleName, string smallIcon)
+		//		{
+		//			ComboBoxMemberData cbxd = new ComboBoxMemberData(internalName, visibleName);
+		//
+		//			cbxd.Image = RibbonUtil.GetBitmapImage(smallIcon);
+		//
+		//			return cbxd;
+		//		}
+		//
+		//		
+		//		private bool AddStackedPushButtonsAndTextBox(RibbonPanel rp)
+		//		{
+		//			TextBoxData tbd = new TextBoxData("TopoSurfaceName");
+		//			PushButtonData[] pbd = new PushButtonData[1];
+		//
+		//			pbd[0] = createButton("RaiseLowerPoints", "Raise\nLower\nPoints", "RaiseLowerPoints", 
+		//				"Raise or Lower points by a fixed amount", SMALLICON, LARGEICON);
+		//
+		//			IList<RibbonItem> ribbonItems = rp.AddStackedItems(tbd, pbd[0]);
+		//
+		//			TopoName = ribbonItems[0] as Autodesk.Revit.UI.TextBox;
+		//			TopoName.Value = "";
+		//			TopoName.ToolTip = "Current Topo Surface Name";
+		//			TopoName.Width = 200.0;
+		//			TopoName.Enabled = false;
+		//
+		//			return true;
+		//		}
+		//
+		//		private void SetTextBoxValue(object sender, TextBoxEnterPressedEventArgs args)
+		//		{
+		//			Units units = new Units(UnitSystem.Imperial);
+		//			double length = 0;
+		//			bool result = UnitFormatUtils.TryParse(units, UnitType.UT_Length, ElevChange.Value.ToString(), out length);
+		//
+		//			if (result)
+		//			{
+		//				elevChangeValue = length;
+		//
+		//				FormatOptions fOpt = new FormatOptions(DisplayUnitType.DUT_DECIMAL_FEET, 0.001);
+		//				fOpt.SuppressTrailingZeros = true;
+		//
+		//
+		//				FormatValueOptions opt = new FormatValueOptions();
+		//				opt.AppendUnitSymbol = true;
+		//				opt.SetFormatOptions(fOpt);
+		//				ElevChange.Value = UnitFormatUtils.Format(units, UnitType.UT_Length, length, false, true, opt);
+		//			}
+		//			else
+		//			{
+		//				ElevChange.Value = "invalid";
+		////				TaskDialog.Show("Parse", "Worked!", TaskDialogCommonButtons.Ok);
+		//				MessageBox.Show("Elevation Change Value", "Amount is not a real distance", MessageBoxButtons.OK, MessageBoxIcon.Error);
+		//			}
+		//		}
+		//
+		//
+		private bool AddStackedPullDownhButtons(RibbonPanel rp)
 		{
-			try
-			{
-				ComboBoxData cbxData1 = new ComboBoxData("functions");
-				ComboBoxData cbxData2 = new ComboBoxData("autoactivate");
+			SplitButton pb0;
+			SplitButton pb1;
 
-				IList<RibbonItem> ris = rp.AddStackedItems(cbxData1, cbxData2);
+			SplitButtonData pdData0 = new SplitButtonData("pullDownButton0", "function select");
+			pdData0.Image = RibbonUtil.GetBitmapImage(SMALLICON);
 
-				ComboBox cbx0 = ris[0] as ComboBox;
-				ComboBox cbx1 = ris[1] as ComboBox;
+			SplitButtonData pdData1 = new SplitButtonData("pullDownButton1", "auto activate");
+			pdData1.Image = RibbonUtil.GetBitmapImage(SMALLICON);
 
-				cbx0.ItemText = "combobox 0";
-				cbx0.ToolTip = "select a function";
-				cbx0.LongDescription = "select a window organize function";
 
-				cbx1.ItemText = "combobox 1";
-				cbx1.ToolTip = "toggle auto activate";
-				cbx1.LongDescription = " toggle auto activate on or off";
+			IList<RibbonItem> ris = rp.AddStackedItems(pdData0, pdData1);
 
-				CreateFunctionsCbx(ref cbx0);
-				CreateAutoActivateCbx(ref cbx1);
+			pb0 = ris[0] as SplitButton;
+			pb1 = ris[1] as SplitButton;
 
-				cbx0.CurrentChanged += Cbx0_CurrentChanged;
-				cbx1.CurrentChanged += Cbx1_CurrentChanged;
+			PushButtonData pbd;
 
-			}
-			catch
-			{
-				return false;
-			}
+			// pull down button 0
+			pbd = createButton("button00", "text 00", "OrganizeProperCascade",
+				"Organize by Proper Cascade", SMALLICON, LARGEICON);
+			pb0.AddPushButton(pbd);
+
+			pbd = createButton("button01", "text 01", "OrganizeWindowsCascade",
+				"Organize by Windows Cascade", SMALLICON, LARGEICON);
+			pb0.AddPushButton(pbd);
+
+			pbd = createButton("button02", "text 02", "OrganizeRight",
+				"Organize by Active Window on the Right", SMALLICON, LARGEICON);
+			pb0.AddPushButton(pbd);
+
+			// pull down button 1
+			pbd = createButton("button10", "text 10", "AutoActivateOn",
+				"Turn on AutoActivate", SMALLICON, LARGEICON);
+			pb1.AddPushButton(pbd);
+
+			pbd = createButton("button11", "text 11", "AutoActivateOff",
+				"Turn off AutoActivate", SMALLICON, LARGEICON);
+			pb1.AddPushButton(pbd);
+
+
 			return true;
 		}
 
-		private void Cbx1_CurrentChanged(object sender, 
-			Autodesk.Revit.UI.Events.ComboBoxCurrentChangedEventArgs e)
+
+
+
+		//		private bool AddStackedPushButtons(RibbonPanel rp)
+		//		{
+		//			PushButtonData[] pbd = new PushButtonData[2];
+		//
+		//			pbd[0] = createButton("RaisePoints2", "Raise\nPoints", "RaisePoints", 
+		//				"Raise points by a fixed amount", SMALLICON, LARGEICON);
+		//
+		//			pbd[1] = createButton("OffsetPoints2", "Offset\nPoints", "OffsetPoints", 
+		//				"Move points by a fixed amount", SMALLICON, LARGEICON);
+		//
+		//			IList<RibbonItem> ribbonItems = rp.AddStackedItems(pbd[0], pbd[1]);
+		//
+		//			return true;
+		//		}
+		//
+		//
+		//		// add a set of pull down buttons (3)
+		//		private bool AddPullDownButton(RibbonPanel ribbonPanel)
+		//		{
+		//			PulldownButton pb;
+		//
+		//			PulldownButtonData pdData = new PulldownButtonData("pullDownButton1", "Edit Points");
+		//			pdData.Image = RibbonUtil.GetBitmapImage(SMALLICON);
+		//
+		//			pb = ribbonPanel.AddItem(pdData) as PulldownButton;
+		//
+		//			PushButtonData pbd;
+		//
+		//			pbd = createButton("RaisePoints1", "Raise Points", "RaisePoints", 
+		//				"Raise points by a fixed amount", SMALLICON, LARGEICON);
+		//			pb.AddPushButton(pbd);
+		//
+		//			pbd = createButton("OffsetPoints1", "Offset Points", "OffsetPoints", 
+		//				"Move points by a fixed amount", SMALLICON, LARGEICON);
+		//			pb.AddPushButton(pbd);
+		//
+		//			return true;
+		//		}
+
+		private static bool x = false;
+
+		private PushButtonData createButton(string ButtonName, string ButtonText, 
+			string className, string ToolTip, string smallIcon, string largeIcon)
 		{
-			TaskDialog.Show("Auto Activate", "this is a test " + e.NewValue.ItemText
-				+ " name (" + e.NewValue.Name + ")");
+			PushButtonData pbd;
+
+			try
+			{
+				pbd = new PushButtonData(ButtonName, ButtonText, AddInPath, string.Concat(CLASSPATH, className))
+				{
+					Image = RibbonUtil.GetBitmapImage(smallIcon),
+					LargeImage = RibbonUtil.GetBitmapImage(largeIcon),
+					ToolTip = ToolTip
+				};
+			}
+			catch (Exception e)
+			{
+				if (!x)
+				{
+					TaskDialog.Show("make button", "failed");
+					x = true;
+				}
+				return null;
+			}
+
+			return pbd;
 		}
-
-		private void Cbx0_CurrentChanged(object sender, 
-			Autodesk.Revit.UI.Events.ComboBoxCurrentChangedEventArgs e)
-		{
-			TaskDialog.Show("Auto Activate", "this is a test " + e.NewValue.ItemText
-				+ " name (" + e.NewValue.Name + ")");
-		}
-
-		private void CreateFunctionsCbx(ref ComboBox cbx)
-		{
-			cbx.AddItem(createCbxMemberData("A", "Proper Cascade", SMALLICON));
-			cbx.AddItem(createCbxMemberData("B", "Window Cascade", SMALLICON));
-			cbx.AddItem(createCbxMemberData("C", "Active at Right", SMALLICON));
-			cbx.AddItem(createCbxMemberData("D", "Active at Top", SMALLICON));
-			cbx.AddItem(createCbxMemberData("E", "Active at Left", SMALLICON));
-			cbx.AddItem(createCbxMemberData("F", "Active at Bottom", SMALLICON));
-		}
-
-		private void CreateAutoActivateCbx(ref ComboBox cbx)
-		{
-			cbx.AddItem(createCbxMemberData("cx0", "Auto Activate On", SMALLICON));
-			cbx.AddItem(createCbxMemberData("cx1", "Auto Activate Off", SMALLICON));
-		}
-
-		private ComboBoxMemberData createCbxMemberData(string internalName, 
-			string visibleName, string smallIcon)
-		{
-			ComboBoxMemberData cbxd = new ComboBoxMemberData(internalName, visibleName);
-
-			cbxd.Image = RibbonUtil.GetBitmapImage(smallIcon);
-
-			return cbxd;
-		}
-
-		
-//		private bool AddStackedPushButtonsAndTextBox(RibbonPanel rp)
-//		{
-//			TextBoxData tbd = new TextBoxData("TopoSurfaceName");
-//			PushButtonData[] pbd = new PushButtonData[1];
-//
-//			pbd[0] = createButton("RaiseLowerPoints", "Raise\nLower\nPoints", "RaiseLowerPoints", 
-//				"Raise or Lower points by a fixed amount", SMALLICON, LARGEICON);
-//
-//			IList<RibbonItem> ribbonItems = rp.AddStackedItems(tbd, pbd[0]);
-//
-//			TopoName = ribbonItems[0] as Autodesk.Revit.UI.TextBox;
-//			TopoName.Value = "";
-//			TopoName.ToolTip = "Current Topo Surface Name";
-//			TopoName.Width = 200.0;
-//			TopoName.Enabled = false;
-//
-//			return true;
-//		}
-//
-//		private void SetTextBoxValue(object sender, TextBoxEnterPressedEventArgs args)
-//		{
-//			Units units = new Units(UnitSystem.Imperial);
-//			double length = 0;
-//			bool result = UnitFormatUtils.TryParse(units, UnitType.UT_Length, ElevChange.Value.ToString(), out length);
-//
-//			if (result)
-//			{
-//				elevChangeValue = length;
-//
-//				FormatOptions fOpt = new FormatOptions(DisplayUnitType.DUT_DECIMAL_FEET, 0.001);
-//				fOpt.SuppressTrailingZeros = true;
-//
-//
-//				FormatValueOptions opt = new FormatValueOptions();
-//				opt.AppendUnitSymbol = true;
-//				opt.SetFormatOptions(fOpt);
-//				ElevChange.Value = UnitFormatUtils.Format(units, UnitType.UT_Length, length, false, true, opt);
-//			}
-//			else
-//			{
-//				ElevChange.Value = "invalid";
-////				TaskDialog.Show("Parse", "Worked!", TaskDialogCommonButtons.Ok);
-//				MessageBox.Show("Elevation Change Value", "Amount is not a real distance", MessageBoxButtons.OK, MessageBoxIcon.Error);
-//			}
-//		}
-//
-//
-//		private bool AddStackedPushButtons(RibbonPanel rp)
-//		{
-//			PushButtonData[] pbd = new PushButtonData[2];
-//
-//			pbd[0] = createButton("RaisePoints2", "Raise\nPoints", "RaisePoints", 
-//				"Raise points by a fixed amount", SMALLICON, LARGEICON);
-//
-//			pbd[1] = createButton("OffsetPoints2", "Offset\nPoints", "OffsetPoints", 
-//				"Move points by a fixed amount", SMALLICON, LARGEICON);
-//
-//			IList<RibbonItem> ribbonItems = rp.AddStackedItems(pbd[0], pbd[1]);
-//
-//			return true;
-//		}
-//
-//
-//		// add a set of pull down buttons (3)
-//		private bool AddPullDownButton(RibbonPanel ribbonPanel)
-//		{
-//			PulldownButton pb;
-//
-//			PulldownButtonData pdData = new PulldownButtonData("pullDownButton1", "Edit Points");
-//			pdData.Image = RibbonUtil.GetBitmapImage(SMALLICON);
-//
-//			pb = ribbonPanel.AddItem(pdData) as PulldownButton;
-//
-//			PushButtonData pbd;
-//
-//			pbd = createButton("RaisePoints1", "Raise Points", "RaisePoints", 
-//				"Raise points by a fixed amount", SMALLICON, LARGEICON);
-//			pb.AddPushButton(pbd);
-//
-//			pbd = createButton("OffsetPoints1", "Offset Points", "OffsetPoints", 
-//				"Move points by a fixed amount", SMALLICON, LARGEICON);
-//			pb.AddPushButton(pbd);
-//
-//			return true;
-//		}
-
-//		private PushButtonData createButton(string ButtonName, string ButtonText, 
-//			string className, string ToolTip, string smallIcon, string largeIcon)
-//		{
-//			PushButtonData pbd;
-//
-//			try
-//			{
-//				pbd = new PushButtonData(ButtonName, ButtonText, AddInPath, string.Concat(CLASSPATH, className))
-//				{
-//					Image = RibbonUtil.GetBitmapImage(smallIcon),
-//					LargeImage = RibbonUtil.GetBitmapImage(largeIcon),
-//					ToolTip = ToolTip
-//				};
-//			}
-//			catch (Exception e)
-//			{
-//				return null;
-//			}
-//
-//			return pbd;
-//		}
 
 		// process when shutting down
 		public Result OnShutdown(UIControlledApplication a)
---
+++ Windows/RwCommands.cs
@@ -0,0 +1,72 @@
+﻿#region Using directives
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+#endregion
+
+// itemname:	RwFunctions
+// username:	jeffs
+// created:		12/3/2017 4:47:52 PM
+
+
+namespace RevitWindows
+{
+	[Transaction(TransactionMode.Manual)]
+	class OrganizeRevitWindows : IExternalCommand
+	{
+		public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+		{
+			string className = this.GetType().Name;
+
+			TaskDialog.Show("Organize Revit Windows", "Class Name| " + className);
+
+			return Result.Succeeded;
+		}
+	}
+
+	[Transaction(TransactionMode.Manual)]
+	class OrganizeProperCascade : OrganizeRevitWindows { }
+
+	[Transaction(TransactionMode.Manual)]
+	class OrganizeWindowsCascade : OrganizeRevitWindows { }
+
+	[Transaction(TransactionMode.Manual)]
+	class OrganizeRight : OrganizeRevitWindows { }
+
+	[Transaction(TransactionMode.Manual)]
+	class OrganizeTop : OrganizeRevitWindows { }
+
+	[Transaction(TransactionMode.Manual)]
+	class OrganizeLeft : OrganizeRevitWindows { }
+
+	[Transaction(TransactionMode.Manual)]
+	class OrganizeBottom : OrganizeRevitWindows { }
+
+	[Transaction(TransactionMode.Manual)]
+	class OrganizeRightOverlapped : OrganizeRevitWindows { }
+
+	class ToggleAutoActivate : IExternalCommand
+	{
+		public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+		{
+			string className = this.GetType().Name;
+
+			TaskDialog.Show("Organize Revit Windows", "Class Name| " + className);
+
+			return Result.Succeeded;
+		}
+	}
+
+	[Transaction(TransactionMode.Manual)]
+	class AutoActivateOn : ToggleAutoActivate { }
+
+	[Transaction(TransactionMode.Manual)]
+	class AutoActivateOff : ToggleAutoActivate { }
+
+}
